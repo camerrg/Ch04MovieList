d05538b80669bf12cddb3fcb520b1188557152af
camerrg <rcameronschoolwork@gmail.com>
2020-06-14 11:35:11 -0500

    WebApplicationTest
    Web Application with Testing

---
+++ WebApplication1Cameron/WebApplication1Cameron/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1Cameron.Models;
+
+namespace WebApplication1Cameron.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            PersonModel person = new PersonModel();
+
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult Index(PersonModel person)
+        {
+            return View(person);
+        }
+
+        public ActionResult CalculateByDate()
+        {
+            PersonExactDateModel person = new PersonExactDateModel();
+
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult CalculateByDate(PersonExactDateModel personModel)
+        {
+            return View(personModel);
+        }
+
+    }
+}
---
+++ WebApplication1Cameron/WebApplication1Cameron/Models/PersonModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1Cameron.Models
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+
+        public int BirthYear { get; set; }
+
+        public string AgeThisYear()
+        {
+            int CurrentYear = DateTime.Today.Year;
+
+            //Validate User Input
+            if (BirthYear > CurrentYear)
+            {
+                return "The birth year can not be after the current year";
+            }
+
+            if (BirthYear == 0)
+            {
+                return "The birth year was not provided";
+            }
+
+            int CurrentAge = CurrentYear - BirthYear;
+
+            return $"{Name} is {CurrentAge} years old";
+        }
+    }
+}
